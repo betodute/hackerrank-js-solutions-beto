c9d988497ac3034d783370f34467c94c3ef26c08
Alberto Duarte <betodute@gmail.com>
2022-11-18 13:40:42 -0800

    Update 061-service-lane.js
    single update

--- beto-solutions/061-service-lane.js
+++ beto-solutions/061-service-lane.js
@@ -1,8 +1,8 @@
 function serviceLane(width, cases) {
   let widestVehicles = [];
   cases.forEach((casePair) => {
-      let laneSegment = width.slice(casePair[0], casePair[1] + 1);
-      widestVehicles.push(Math.min(...laneSegment));
+    let laneSegment = width.slice(casePair[0], casePair[1] + 1);
+    widestVehicles.push(Math.min(...laneSegment));
   });
   return widestVehicles;
 };
